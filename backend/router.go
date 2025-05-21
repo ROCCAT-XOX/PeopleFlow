64d41e5e12e07eac40e1216d597826b81aef566c
Yannick Stumpf <yannick.stumpf@smart3ch.de>
2025-05-21 15:37:15 +0200

    fixed an removed some bugs

--- backend/router.go
+++ backend/router.go
@@ -22,6 +22,20 @@ func InitializeRoutes(router *gin.Engine) {
 		panic("Fehler beim Verbinden zur Datenbank")
 	}
 
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4321")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	// Public routes (keine Authentifizierung erforderlich)
 	router.GET("/login", func(c *gin.Context) {
 		// Token aus dem Cookie extrahieren
